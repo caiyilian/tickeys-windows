93846d55d6b5dd2c90cc102d848877fc014fc9fc
caiyilian <tomoya20021026@163.com>
2026-06-15 22:11:24 +0800

    chore: bump version to 1.0.3

--- scripts/package-release.ps1
+++ scripts/package-release.ps1
@@ -3,7 +3,7 @@
 
 $ErrorActionPreference = "Stop"
 
-$version = "1.0.2"
+$version = "1.0.3"
 $releaseDir = "target\release"
 $packageName = "tickeys-windows-v$version"
 $packageDir = "target\$packageName"
